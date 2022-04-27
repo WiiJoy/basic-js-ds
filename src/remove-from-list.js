eb6fdcc703743e382675c21982c6660a3a2a0b8b
WiiJoy MacOS <wiijoy@bk.ru>
2022-04-27 21:34:52 +0300

    feat: done remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let currListItem = l
+
+  if (currListItem.value === k) {
+    currListItem = currListItem.next
+    l = l.next
+  }
+
+  while (currListItem.next !== null) {
+    if (currListItem.next.value === k) {
+      currListItem.next = currListItem.next.next
+    } else {
+      currListItem = currListItem.next
+    }
+  }
+
+  return l
 }
 
 module.exports = {
